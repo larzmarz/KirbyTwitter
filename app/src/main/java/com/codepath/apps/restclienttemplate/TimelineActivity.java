bdbdca2ee887c28780d45951e9e208e3fcbda048
larzmarz <laurys3577@gmail.com>
2022-06-07 17:05:53 -0700

    first commit/ got up to step 6 today

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -51,6 +51,8 @@ public class TimelineActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 onLogoutButton();
+                client.clearAccessToken();
+
             }
         });
 
